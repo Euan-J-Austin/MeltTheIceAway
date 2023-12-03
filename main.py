6c2a265933b52dd2326438dc7b8a143a27cc5b49
Euan-J-Austin <124497636+Euan-J-Austin@users.noreply.github.com>
2023-12-03 07:28:40 +0000

    QUESTION 16.

--- main.py
+++ main.py
@@ -274,8 +274,22 @@ Then, the output should be:
 11106
 """
 
-def adder(a):
-  return print(sum(int(a*i) for i in range(1,5)))
+# def adder(a):
+#   return print(sum(int(a*i) for i in range(1,5)))
 
-adder('9')
-    
+# adder('9')
+
+"""
+16 Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers. >Suppose the following input is supplied to the program:
+
+1,2,3,4,5,6,7,8,9
+Then, the output should be:
+
+1,9,25,49,81
+"""
+
+l = [1,2,3,4,5,6,7,8,9]
+
+out = [x*x for x in l if x%2 != 0]
+
+print(out)
