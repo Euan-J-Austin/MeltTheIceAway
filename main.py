c6ef24888b959a6a49c1cf86e1aa3a78d8a8b01a
Euan-J-Austin <124497636+Euan-J-Austin@users.noreply.github.com>
2023-12-10 07:57:56 +0000

    QUESTION 23 AND 24.

--- main.py
+++ main.py
@@ -499,6 +499,42 @@ to:1
 
 """
 
-input = "New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3."
+# input = "New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3."
 
-print({x: input.count(x) for x in sorted(input.split(' '))})
+# print({x: input.count(x) for x in sorted(input.split(' '))})
+
+"""
+23
+
+Write a method which can calculate square value of number
+
+
+"""
+
+# class Calculator:
+#   def get_number():
+#     return(input('ENTER NUMBER: '))
+#   def print_squared():
+#     return print(int(Calculator.get_number())**2)
+
+# m = Calculator
+# m.print_squared()
+
+"""
+24
+
+Python has many built-in functions, and if you do not know how to use it, you can read document online or find some books. But Python has a built-in document function for every built-in functions.
+
+Please write a program to print some Python built-in functions documents, such as abs(), int(), raw_input()
+
+And add document for your own function
+"""
+
+def doc_printer():
+  """Prints __doc__ information"""
+  print(abs.__doc__)
+  print(int.__doc__)
+  print(doc_printer.__doc__)
+  
+
+doc_printer()
