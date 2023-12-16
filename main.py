c23a19fc8061984dadc8f6d3172177378ed952ae
Euan-J-Austin <124497636+Euan-J-Austin@users.noreply.github.com>
2023-12-16 23:56:19 +0000

    QUESTIONS 47 AND 48.

--- main.py
+++ main.py
@@ -758,4 +758,62 @@ Define a class named American and its subclass NewYorker.
 
 
 # newYorker.printNationality()
+
+"""
+47
+
+Define a class named Circle which can be constructed by a radius. The Circle class has a method which can compute the area.
+
+"""
+
+# import math
+
+# class Circle:
+#   def __init__(self, radius):
+#     self.radius = int(radius)
+#   def area(self):
+#     return print(round(math.pi * self.radius**2, 2))
+
+# Circle(input("Radius: ")).area()
+
+"""
+48 
+
+
+Define a class named Rectangle which can be constructed by a length and width. The Rectangle class has a method which can compute the area.
+
+"""
+
+
+# class Rectangle:
+#   def __init__(self, l, w):
+#     self.length = l
+#     self.width = w
+#   def area(self):
+#     return print(self.length*self.width)
+
+# Rectangle(10,12).area()
+
+"""
+49
+
+Define a class named Shape and its subclass Square. The Square class has an init function which takes a length as argument. Both classes have a area function which can print the area of the shape where Shape's area is 0 by default.
+
+"""
+
+class Shape:
+  def __init__(self, length):
+    self.l = length
+  def area(self):
+    return print(self.l*self.l)
+
+class Square(Shape):
+  def __init__(self, area):
+    super().__init__()
     
+
+
+Shape(6)
+Square.area()
+  
+  
